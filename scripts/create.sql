18517105a5e350002685b3912870e0a14deb77cf
Alex Dwivedi <alexdwivedi@gmail.com>
2019-04-27 23:43:27 -0400

    Removed employee fk from reservations

--- scripts/create.sql
+++ scripts/create.sql
@@ -47,14 +47,13 @@ create table Reservations (
     reservation_id integer not null generated always as identity (start with 1, increment by 1),
     guest_id integer not null,
     room_id integer not null,
-    employee_id integer not null,
     check_in_date date not null,
     check_out_date date not null,
     total decimal(7, 2) not null,
+    occupants integer not null,
     constraint PK_Reservations primary key (reservation_id),
     constraint FK_Reservations_Guest foreign key (guest_id) references Guests(guest_id) on delete cascade,
-    constraint FK_Reservations_Room foreign key (room_id) references Rooms(room_id) on delete cascade,
-    constraint FK_Reservations_Employee foreign key (employee_id) references Employees(employee_id) on delete cascade
+    constraint FK_Reservations_Room foreign key (room_id) references Rooms(room_id) on delete cascade
 );
 
 create table Amenities (
